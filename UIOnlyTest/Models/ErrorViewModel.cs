e51c59bc3928c0b041cf2f82401b645f9188f0c5
Andy Badera <ABadera@3cloudsolutions.com>
2018-09-20 11:30:45 -0500

    Add project files.

---
+++ UIOnlyTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UIOnlyTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
